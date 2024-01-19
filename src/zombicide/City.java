d58e6cf2e4ad7bda53b832726fab2250f88f7040
elies.placide.etu <elies.placide.etu@a10p20.fil.univ-lille.fr>
2024-01-19 10:35:50 +0100

    add City class and its attributes

---
+++ src/zombicide/City.java
@@ -0,0 +1,20 @@
+package zombicide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class City{
+
+
+    private int width;
+    private int height;
+    private List<Street> streets;
+    private List<Building> buildings;
+    private List<Area> areas;
+    
+
+	
+
+	
+    
+}
