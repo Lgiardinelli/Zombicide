5e690f8f5c8d73c4fed31a809ce41398ae719f96
dylan.lemaire2.etu <dylan.lemaire2.etu@univ-lille.fr>
2024-01-19 16:22:44 +0100

    init Room.java

---
+++ src/zombicide/Room.java
@@ -0,0 +1,38 @@
+package zombicide;
+
+/**
+ * The {@code Room} class represents a room in the Zombicide game.
+ * Each room can have multiple doors.
+ */
+public class Room {
+    
+    /**
+     * An array of doors in the room.
+     */
+    private final Door[] doors;
+    
+    /**
+     * Constructs a room with the specified number of doors.
+     * 
+     * @param nbDoors The number of doors in the room.
+     */
+    public Room(int nbDoors) {
+        this.doors = new Door[nbDoors];
+        
+        for (int i = 0; i < this.doors.length; i++) {
+            this.doors[i] = new Door();
+        }
+    }
+    
+    /**
+     * Gets the door at the specified index in the room.
+     * 
+     * @param index The index of the door to retrieve.
+     * @return The door at the specified index.
+     * @throws IndexOutOfBoundsException If the index is out of range.
+     */
+    public Door getDoor(int index) {
+        return this.doors[index];
+    }
+}
+
