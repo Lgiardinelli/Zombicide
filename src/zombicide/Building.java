54700fff19feb653724c373e5fd71697f7c90a5e
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-01-19 11:03:07 +0100

    Add attributes width, height, rooms

---
+++ src/zombicide/Building.java
@@ -0,0 +1,14 @@
+package zombicide;
+
+import java.util.ArrayList;
+
+public class Building {
+	/** The width of the building */
+	private int width;
+	
+	/** The height of the building */
+	private int height;
+	
+	/** List of the room in the building */
+	private List<Room> rooms;
+}
