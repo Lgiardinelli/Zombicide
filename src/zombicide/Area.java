1c71a201672484c07cfe69fda4ceec5b69fc93d0
leo.giardinelli.etu <leo.giardinelli.etu@a11p1>
2024-01-19 10:40:37 +0100

    Add area class attributes

--- src/zombicide/Area.java
+++ src/zombicide/Area.java
@@ -1,5 +1,11 @@
 package zombicide;
 
 public class Area {
-
+	
+	private int posX;
+	private int posY;
+	private List<Survivor> survivors;
+	private List<Zombie> zombies;
+	private int noise;
+	
 }
