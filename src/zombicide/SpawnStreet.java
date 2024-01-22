5c1be16dcbc20a95290b546fe33df6c12bce53d7
dylan.lemaire2.etu <dylan.lemaire2.etu@univ-lille.fr>
2024-01-22 10:00:06 +0100

    add position to Street.java, SpawnStreet.java

--- src/zombicide/SpawnStreet.java
+++ src/zombicide/SpawnStreet.java
@@ -1,5 +1,8 @@
 package zombicide;
 
 public class SpawnStreet extends Street {
-
+	
+	public SpawnStreet(int posX, int posY) {
+		super(posX, posY);
+	}
 }
--- src/zombicide/Street.java
+++ src/zombicide/Street.java
@@ -2,4 +2,7 @@ package zombicide;
 
 public class Street extends Area {
 
+	public Street(int posX, int posY) {
+		super(posX, posY);
+	}
 }
