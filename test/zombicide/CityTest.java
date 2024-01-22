398df4d12efb3abf5cdd876104e737f08c1e5a17
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-01-22 09:52:11 +0100

    Add method test checkIfCityCanBeSplit

--- test/zombicide/CityTest.java
+++ test/zombicide/CityTest.java
@@ -5,5 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class CityTest {
-	
+
+	@Test
+	public void checkIfCityCanBeSplit() {
+		City city = new City(5, 5);
+		city.createCity();
+		assertTrue(city.canBeSplit());
+	}
 }
