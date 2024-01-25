de43cc2d1df675e0f1f851868c33c19e1f541010
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-01-25 22:30:06 +0100

    Add areaInitialization method

--- test/zombicide/AreaTest.java
+++ test/zombicide/AreaTest.java
@@ -5,5 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class AreaTest {
-	
+	@Test
+    void testAreaInitialization() {
+        Area area = new Room(1, 2, 4);
+        assertNotNull(area);
+        assertEquals(1, area.getX());
+        assertEquals(2, area.getY());
+    }
+
 }
