aef8b27ff973573bddaae9b53692391a7eb2fab3
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-01-25 22:48:54 +0100

    Update test method areaInitialization, AreaSurvivorList, AreaZombieList

--- test/zombicide/AreaTest.java
+++ test/zombicide/AreaTest.java
@@ -7,22 +7,68 @@ import org.junit.jupiter.api.Test;
 class AreaTest {
 	@Test
     void testAreaInitialization() {
-        Area area = new Room(1, 2, 4);
-        assertNotNull(area);
-        assertEquals(1, area.getX());
-        assertEquals(2, area.getY());
+		//Room
+        Area room = new Room(1, 2, 4);
+        assertNotNull(room);
+        assertEquals(1, room.getX());
+        assertEquals(2, room.getY());
+        
+        //Street
+        Area street = new Street(4,5);
+        assertNotNull(street);
+        assertEquals(4, street.getX());
+        assertEquals(5, street.getY());
     }
 
 	@Test
     void testAreaSurvivorList() {
-        Area area = new Room(1, 2, 4);
-        assertNotNull(area);
-        assertTrue(area.getSurvivors().isEmpty());
+		//Room
+        Area room = new Room(1, 2, 4);
+        assertNotNull(room);
+        assertTrue(room.getSurvivors().isEmpty());
 
         Survivor survivor = new Survivor("John");
-        area.addSurvivor(survivor);
+        room.addSurvivor(survivor);
 
-        assertFalse(area.getSurvivors().isEmpty());
-        assertTrue(area.getSurvivors().contains(survivor));
+        assertFalse(room.getSurvivors().isEmpty());
+        assertTrue(room.getSurvivors().contains(survivor));
+        
+        //Street
+        Area street = new Street(1, 2, 4);
+        assertNotNull(street);
+        assertTrue(street.getSurvivors().isEmpty());
+
+        Survivor survivor = new Survivor("Elies");
+        street.addSurvivor(survivor);
+
+        assertFalse(street.getSurvivors().isEmpty());
+        assertTrue(street.getSurvivors().contains(survivor));
     }
+	
+	@Test
+    void testAreaZombieList() {
+		//Room
+        Area room = new Room(1, 2, 4);
+        assertNotNull(room);
+        assertTrue(room.getZombies().isEmpty());
+
+        Zombie zombie = new Zombie();
+        room.addZombie(zombie);
+
+        assertFalse(room.getZombies().isEmpty());
+        assertTrue(room.getZombies().contains(zombie));
+        
+        //Street
+        Area street = new Street(1, 2, 4);
+        assertNotNull(street);
+        assertTrue(street.getZombies().isEmpty());
+
+        Zombie zombie = new Zombie();
+        street.addZombie(zombie);
+
+        assertFalse(street.getZombies().isEmpty());
+        assertTrue(street.getZombies().contains(zombie));
+	}
+	
+	
 }
