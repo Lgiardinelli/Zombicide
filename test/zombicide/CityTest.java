ad7e7a49193f39d121a27942fa809e320c5c78ca
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-01-26 12:07:52 +0100

    Update and add addRoom test method (not finish)

--- test/zombicide/CityTest.java
+++ test/zombicide/CityTest.java
@@ -7,14 +7,9 @@ import org.junit.jupiter.api.Test;
 class CityTest {
 
 	@Test
-	void testCityInitialization() {
-		City city = new City(10, 11);
-		assertNotNull(city);
-	}
-	
-	@Test
-	void testCityAreaInitialization() {
-		City city = new City(10,11);
-		assertNotNull(city.getArea(5, 5));
+	void testAddRoom() {
+		City city = new City(10, 10);
+		Room room = new Room(4, 5, 4);
+		city.addRoom(room);
 	}
 }
