7ee29a49d39dd28fc3413c78482de0ba3d938ab2
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-01-30 17:24:22 +0100

    Update RoomTest : remoove method and add testRoomInitialization method

--- test/zombicide/RoomTest.java
+++ test/zombicide/RoomTest.java
@@ -8,9 +8,8 @@ class RoomTest {
 
 	@Test
 	void testRoomInitialization() {
-		City city = new City(10,10);
-		Room room = new Room(5, 6, 4);
-		city.addRoom(room);
-		assertNotNull(city.getArea(5, 6));
+		Room room = new Room(5, 6);
+		assertNotNull(room);
 	}
+
 }
