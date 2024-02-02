8a6015ad0174110faa86bfed003f7e9996459747
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-02 16:32:52 +0100

    Implementation of beforeEach

--- test/zombicide/RoomTest.java
+++ test/zombicide/RoomTest.java
@@ -2,10 +2,18 @@ package zombicide;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class RoomTest {
 
+	private Room room;
+	
+	@BeforeEach
+	private void before() {
+		room = new Room(5,6);
+	}
+	
 	@Test
 	void testRoomInitialization() {
 		Room room = new Room(5, 6);
