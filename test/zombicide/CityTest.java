bd08931ef0114fde2f71fa827eae364d2edc3e1f
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-02 16:29:06 +0100

    Implementation of BeforeEach

--- test/zombicide/CityTest.java
+++ test/zombicide/CityTest.java
@@ -2,10 +2,18 @@ package zombicide;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CityTest {
 
+	private City city;
+	
+	@BeforeEach
+	private void before() {
+		city = new City(10,10);
+	}
+	
 	@Test
 	void testInitializationCity() {
 		City city = new City(10,10);
