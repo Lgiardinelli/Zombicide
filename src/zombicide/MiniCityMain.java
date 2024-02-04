0cb1f9146418a60f0ebdb415ba92d674c7e20904
leo.giardinelli.etu <leo.giardinelli.etu@a11p1>
2024-02-04 16:31:35 +0100

    Update miniCity

--- src/zombicide/MiniCityMain.java
+++ src/zombicide/MiniCityMain.java
@@ -3,13 +3,16 @@ package zombicide;
 public class MiniCityMain {
 
 	public static void main(String[] args) {
-		 City aCity = new City(10,10);
-		 Room r = (Room) aCity.getAreas()[0][0];
-		 r.getDoor(DoorDirection.DOWN).open();
-		 Room r1 = (Room) aCity.getAreas()[0][5];
+		 City aCity = new City(5,5);
+		 Area r = aCity.getAreas()[0][0];
+		 r.getDoor(DoorDirection.RIGHT).open();
+		 Area r1 = aCity.getAreas()[0][4];
 		 r1.getDoor(DoorDirection.DOWN).open();
-		 Room r2 = (Room) aCity.getAreas()[2][6];
-		 r2.getDoor(DoorDirection.DOWN).open();
+//		 Area r2 = aCity.getAreas()[2][6];
+//		 r2.getDoor(DoorDirection.DOWN).open();
+		 Area r3 = aCity.getAreas()[0][1];
+		 r3.getDoor(DoorDirection.DOWN).open();
 		 aCity.display();
 	}
 }
+
