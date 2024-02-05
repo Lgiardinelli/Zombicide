346de1875b7d6006146c60facb3a7e38dcb5d727
elies.placide.etu <elies.placide.etu@a11p5.fil.univ-lille.fr>
2024-02-05 09:04:35 +0100

    add constructor

---
+++ src/zombicide/BackPack.java
@@ -0,0 +1,13 @@
+package zombicide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BackPack {
+	
+	private List<Item> items;
+	
+	public BackPack() {
+		this.items = new ArrayList<>();
+	}
+}
