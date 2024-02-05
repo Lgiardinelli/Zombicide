fb37fd9fe41ec26de4a0a98ee9ce5559922edd5b
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-05 09:58:49 +0100

    Add constructor

--- src/zombicide/Abomination.java
+++ src/zombicide/Abomination.java
@@ -1,5 +1,10 @@
 package zombicide;
 
 public class Abomination extends Zombie {
-
+	public Abomination() {
+		this.attackPoints = 3;
+		this.actionPoints = 1;
+		this.lifePoints = 6;
+		this.isStrong = true;
+	}
 }
