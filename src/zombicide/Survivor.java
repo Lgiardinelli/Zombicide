1ca9953cf94f06f53203b80ed8f133715288e19a
elies.placide.etu <elies.placide.etu@a11p5.fil.univ-lille.fr>
2024-02-05 08:49:43 +0100

    add handleWeapon method

--- src/zombicide/Survivor.java
+++ src/zombicide/Survivor.java
@@ -11,4 +11,11 @@ public class Survivor extends Actor{
 		this.actionPoints = 3;
 		this.lifePoints = 5;
 	}
+	
+	public void handleWeapon(Weapon weapon) {
+		if(this.handleWeapon != null) {
+			this.backpack.addEquipement(this.handleWeapon);
+		}
+		this.handleWeapon = weapon;
+	}
 }
