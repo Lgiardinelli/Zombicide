5253853e92cf7d3eee075990070196c0ff37d203
elies.placide.etu <elies.placide.etu@a11p5.fil.univ-lille.fr>
2024-02-05 09:57:21 +0100

    add shoot method

--- src/zombicide/Weapon.java
+++ src/zombicide/Weapon.java
@@ -1,11 +1,27 @@
 package zombicide;
 
-public class Weapon implements Item{
-	
-	private int nbDiceThrows;
-	private int diceThreshold;
-	private int damage;
-	private int minHittingRange;
-	private int maxHittingRange;
-	
+import java.util.Random;
+
+
+public abstract class Weapon implements Item {
+
+    private int nbDiceThrows;
+    private int diceThreshold;
+    private int damage;
+    private int minHittingRange;
+    private int maxHittingRange;
+
+    /**
+     * Performs the shooting action with the weapon, rolling dice and calculating the result.
+     *
+     * @return The total result obtained by rolling the dice.
+     */
+    public int shoot() {
+        Random random = new Random();
+        int result = 0;
+        for (int i = 0; i < this.nbDiceThrows; i++) {
+            result += random.nextInt(6) + 1;
+        }
+        return result;
+    }
 }
