cdf8e21cd44ba2b6f81afb540ebd9c2ee06e6b00
elies.placide.etu <elies.placide.etu@a11p5.fil.univ-lille.fr>
2024-02-05 09:26:13 +0100

    add addItem and removeItem methods

--- src/zombicide/BackPack.java
+++ src/zombicide/BackPack.java
@@ -4,16 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BackPack {
-	
-	private List<Item> items;
-	
-	public BackPack() {
-		this.items = new ArrayList<>();
-	}
-	
-	
-	public void addItem(Item i) {
-		this.items.add(i);
-	}
-	
+
+    private List<Item> items;
+
+    public BackPack() {
+        this.items = new ArrayList<>();
+    }
+
+    /**
+     * Adds the specified item to the backpack if the backpack is not full.
+     *
+     * @param item The item to be added to the backpack.
+     */
+    public void addItem(Item item) {
+        if (this.items.size() < 5) {
+            this.items.add(item);
+        }
+    }
+
+    /**
+     * Removes the specified item from the backpack if the backpack is not empty.
+     *
+     * @param item The item to be removed from the backpack.
+     */
+    public void removeItem(Item item) {
+        if (this.items.size() >= 1) {
+            this.items.remove(item);
+        }
+    }
 }
