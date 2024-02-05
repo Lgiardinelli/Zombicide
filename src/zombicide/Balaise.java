fa8de8ed30e2d13e1dafc95b00776b270356753a
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-05 09:56:32 +0100

    Add Balaise class and constructor

--- src/zombicide/Balaise.java
+++ src/zombicide/Balaise.java
@@ -1,5 +1,10 @@
 package zombicide;
 
-public class Balaise extends Actor{
-
+public class Balaise extends Zombie{	
+	public Balaise() {
+		this.actionPoints = 1;
+		this.lifePoints = 4;
+		this.attackPoints = 2;
+		this.isStrong = true;
+	}
 }
