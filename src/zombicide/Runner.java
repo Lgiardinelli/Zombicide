3763011da32d3c2ba95ca9a47e94d03d5d8cf989
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-05 09:34:50 +0100

    add constructor

--- src/zombicide/Runner.java
+++ src/zombicide/Runner.java
@@ -1,5 +1,11 @@
 package zombicide;
 
 public class Runner extends Zombie {
+	public Runner() {
+		this.actionPoints = 2;
+		this.attackPoints = 1;
+		this.isStrong = false;
+		this.lifePoints = 1;
+	}
 	
 }
