2bdfe6b4c94a3b2470c888a260f35a357e4b42b0
leo.giardinelli.etu <leo.giardinelli.etu@a11p1>
2024-02-07 21:52:43 +0100

    Update main

--- src/zombicide/Main.java
+++ src/zombicide/Main.java
@@ -3,11 +3,27 @@ package zombicide;
 public class Main {
 
 	public static void main(String[] args) {
+
+		City trainCity = new City(5, 5);
 		
-		City aCity = new City(10, 10);
+		if (args.length == 0) {
+			City aCity = new City(10, 10);
+			aCity.display();
+		}
+		else {
+			int x = Integer.parseInt(args[0]);
+			int y = Integer.parseInt(args[1]);
+			City aCity = new City(x, y);
+			aCity.display();
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Plateau 'entrainement :");
+		trainCity.display();
+		System.out.println("");
 		
 		
-		aCity.display();
 		
 	}
 
