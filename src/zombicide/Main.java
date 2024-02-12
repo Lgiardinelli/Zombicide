1edd43e9ce1718b83beb2ad83fabddf7ed490b87
dylan.lemaire2.etu <dylan.lemaire2.etu@univ-lille.fr>
2024-02-12 09:10:51 +0100

    add test case for opening doors

--- src/zombicide/Main.java
+++ src/zombicide/Main.java
@@ -8,18 +8,29 @@ public class Main {
 		
 		if (args.length == 0) {
 			City aCity = new City(10, 10);
+			
+			aCity.getAreas()[0][0].getDoor(DoorDirection.DOWN).open();
+			aCity.getAreas()[0][1].getDoor(DoorDirection.DOWN).open();
+			aCity.getAreas()[0][1].getDoor(DoorDirection.LEFT).open();
+			
 			aCity.display();
 		}
 		else {
 			int x = Integer.parseInt(args[0]);
 			int y = Integer.parseInt(args[1]);
 			City aCity = new City(x, y);
+			
 			aCity.display();
 		}
 		
 		System.out.println("");
 		System.out.println("");
-		System.out.println("Plateau 'entrainement :");
+		System.out.println("Plateau d'entrainement :");
+		
+		trainCity.getAreas()[0][0].getDoor(DoorDirection.DOWN).open();
+		trainCity.getAreas()[0][1].getDoor(DoorDirection.DOWN).open();
+		trainCity.getAreas()[0][1].getDoor(DoorDirection.LEFT).open();
+		
 		trainCity.display();
 		System.out.println("");
 		
