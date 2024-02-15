bab1e880390ceebeedbe6e4cf49a79cb7ecf034a
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-15 22:36:19 +0100

    Add actorTest class and add before method

---
+++ test/zombicide/ActorTest.java
@@ -0,0 +1,38 @@
+package zombicide;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import zombicide.actor.Survivor;
+import zombicide.actor.Zombie;
+import zombicide.actor.zombie.Abomination;
+import zombicide.actor.zombie.Balaise;
+import zombicide.actor.zombie.Runner;
+import zombicide.actor.zombie.Walker;
+import zombicide.role.Fighter;
+
+public class ActorTest {
+    private Survivor survivor;
+    private Zombie balaise;
+    private Zombie runner;
+    private Zombie walker;
+    private Zombie abomination;
+    private Role fighter;
+
+    @BeforeEach
+    public void before(){
+        this.survivor = new Survivor(fighter);
+        this.balaise = new Balaise();
+        this.abomination = new Abomination();
+        this.walker = new Walker();
+        this.runner = new Runner();
+    }
+
+    @Test
+    void testZombieInitialization(){
+        assertNotNull(balaise);
+        assertNotNull(abomination);
+        assertNotNull(walker);
+        assertNotNull(runner);
+    }
+}
