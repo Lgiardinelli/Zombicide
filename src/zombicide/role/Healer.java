ebf6ecebf1e958d087497c89933c0a2c6af73ab9
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-02-15 23:30:48 +0100

    Add method addLifePoint in the Healer class

--- src/zombicide/role/Healer.java
+++ src/zombicide/role/Healer.java
@@ -1,5 +1,22 @@
 package zombicide.role;
 
+import zombicide.actor.Survivor;
+
+import java.util.List;
+
 public class Healer extends Role {
-	
+
+    private static final int NB_LIFE_POINTS = 1;
+
+    public Healer() {
+        super();
+    }
+
+    public void addLifePoint(List<Survivor> survivors) {
+        for (Survivor survivor : survivors) {
+            survivor.addLifePoints(NB_LIFE_POINTS);
+        }
+    }
+
+
 }
