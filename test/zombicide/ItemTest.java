8effa2309c7efa3c395fd13497bbd1e1e682a11d
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-16 14:24:53 +0100

    Update testGetDiceThreshold method

--- test/zombicide/ItemTest.java
+++ test/zombicide/ItemTest.java
@@ -52,7 +52,8 @@ public class ItemTest {
 
     @Test
     void testGetDiceThreshold(){
-        assertEquals(weapon.getDiceThreshold(), 4);
+        int seuilResult = weapon.getDiceThreshold();
+        assertEquals(seuilResult, 4);
     }
 
     @Test
