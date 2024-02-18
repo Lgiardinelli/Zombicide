613262ce998904ccf0690881ff56fd4f69aa368e
dylanlmr <xeskragd@gmail.com>
2024-02-18 03:15:33 +0100

    add more colors for zombies and survivors

--- src/zombicide/util/Color.java
+++ src/zombicide/util/Color.java
@@ -4,7 +4,9 @@ public enum Color {
     RESET("\u001B[0m"),
     RED("\u001B[31m"),
     BLACK_BOLD("\u001B[1;30m"),
-    BLUE_BOLD_BRIGHT("\u001B[1;94m"),
+    BLUE_BOLD("\u001B[1;34m"),
+    RED_BOLD_BRIGHT("\u001B[1;91m"),
+    GREEN_BOLD_BRIGHT("\u001B[1;92m"),
     CYAN_BOLD_BRIGHT("\u001B[1;96m"),
     GREEN_BG_BRIGHT("\u001B[102m"),
     PURPLE_BG_BRIGHT("\u001B[105m");
