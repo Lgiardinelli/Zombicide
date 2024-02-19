ee7b56b9fef2efc2e8a3cf80e173a9517ea9994e
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-19 11:16:44 +0100

    Add makeNoise method

--- src/zombicide/actor/action/NoiseAction.java
+++ src/zombicide/actor/action/NoiseAction.java
@@ -1,5 +1,21 @@
 package zombicide.actor.action;
 
-public class NoiseAction {
+import zombicide.Area;
+import zombicide.SurvivorAction;
+import zombicide.actor.Survivor;
 
+public class NoiseAction implements SurvivorAction {
+
+    private Survivor survivor;
+
+    /**
+        Increases the noise level in the current area where the survivor is located.
+        If the survivor is not in any area, no noise level is increased.
+     */
+    public void makeNoise(){
+        Area currentArea = survivor.getCurrentArea();
+        if(currentArea != null){
+            currentArea.increaseNoiseLevel();
+        }
+    }
 }
