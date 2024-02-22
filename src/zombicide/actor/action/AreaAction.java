6fb64c11452de26b244f65c3dbee8d946eb091c5
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-22 22:30:55 +0100

    Add lookAround method

--- src/zombicide/actor/action/AreaAction.java
+++ src/zombicide/actor/action/AreaAction.java
@@ -4,4 +4,10 @@ import zombicide.SurvivorAction;
 
 public class AreaAction implements SurvivorAction {
 
+    /**
+     * TODO
+     */
+    public boolean lookAround(){
+        return true;
+    }
 }
