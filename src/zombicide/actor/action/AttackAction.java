8f5210344ec5aec04ee4d5f080261c0947cf9960
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-22 22:43:44 +0100

    add attackAction class

---
+++ src/zombicide/actor/action/AttackAction.java
@@ -0,0 +1,13 @@
+package zombicide.actor.action;
+
+import zombicide.ActorAction;
+
+public class AttackAction implements ActorAction {
+
+    /**
+     * TODO
+     */
+    public boolean attackZombie(){
+        return true;
+    }
+}
