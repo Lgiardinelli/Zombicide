2b69899a4edc32ead8a62d093e4fb2548012a35e
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-22 22:32:28 +0100

    Add takeInHand method

--- src/zombicide/actor/action/BackPackAction.java
+++ src/zombicide/actor/action/BackPackAction.java
@@ -3,4 +3,7 @@ package zombicide.actor.action;
 import zombicide.SurvivorAction;
 
 public class BackPackAction implements SurvivorAction {
+    public boolean takeInHand(){
+        return true;
+    }
 }
