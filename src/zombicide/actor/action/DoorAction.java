c43cb64eb12cc1157a0df06a4313fb6ab696b41b
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-22 22:35:02 +0100

    Add openTheDoor method

--- src/zombicide/actor/action/DoorAction.java
+++ src/zombicide/actor/action/DoorAction.java
@@ -3,4 +3,10 @@ package zombicide.actor.action;
 import zombicide.SurvivorAction;
 
 public class DoorAction implements SurvivorAction {
+    /**
+     * TODO
+     */
+    public boolean openTheDoor(){
+        return true;
+    }
 }
