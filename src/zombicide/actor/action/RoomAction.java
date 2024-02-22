61d4d39c13989b6982192ccfb6e7719d788c4daa
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-22 22:29:28 +0100

    Add searchRoom method

--- src/zombicide/actor/action/RoomAction.java
+++ src/zombicide/actor/action/RoomAction.java
@@ -3,4 +3,11 @@ package zombicide.actor.action;
 import zombicide.SurvivorAction;
 
 public class RoomAction implements SurvivorAction {
+
+    /**
+     * TODO
+     */
+    public boolean searchRoom(){
+        return true;
+    }
 }
