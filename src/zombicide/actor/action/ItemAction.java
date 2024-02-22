5d0013ee9a939e7c9864c985e2165e97af779649
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-02-22 22:33:58 +0100

    Add useEquipement method

--- src/zombicide/actor/action/ItemAction.java
+++ src/zombicide/actor/action/ItemAction.java
@@ -3,4 +3,11 @@ package zombicide.actor.action;
 import zombicide.SurvivorAction;
 
 public class ItemAction implements SurvivorAction {
+
+    /**
+     * TODO
+     */
+    public boolean useEquipement(){
+        return true;
+    }
 }
