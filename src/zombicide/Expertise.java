ee982312e03040bda36eb44729b96f64f494e777
dylanlmr <xeskragd@gmail.com>
2024-02-24 15:28:39 +0100

    add Expertise.java to handle survivors stages

---
+++ src/zombicide/Expertise.java
@@ -0,0 +1,24 @@
+package zombicide;
+
+public enum Expertise {
+    STAGE_THREE(3),
+    STAGE_SEVEN(7),
+    STAGE_ELEVEN(11);
+
+    final int threshold;
+
+    Expertise(int threshold) {
+        this.threshold = threshold;
+    }
+
+    public int getThreshold() {
+        return threshold;
+    }
+
+    public static Expertise getStage(int value) {
+        for (Expertise stage : Expertise.values())
+            if (stage.getThreshold() == value)
+                return stage;
+        return null;
+    }
+}
