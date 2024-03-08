c1acc26b3b322a86cd384e66ba648d61fbc0ee67
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-08 15:12:33 +0100

    update MoveAction class

--- src/zombicide/actor/action/MoveAction.java
+++ src/zombicide/actor/action/MoveAction.java
@@ -1,10 +1,15 @@
 package zombicide.actor.action;
 
-public class MoveAction {
-    /**
-     * TODO
-     */
-    public boolean move(){
-        return true;
+import zombicide.*;
+
+
+public class MoveAction implements ActorAction {
+
+    private ActorDirection direction;
+
+    public Area move(Actor a){
+        Area actorArea = a.getArea();
+        int x
     }
+
 }
