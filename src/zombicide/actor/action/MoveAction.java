226c4fab97f47a51ec5a19db7f9212ac7954cb42
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-08 15:22:56 +0100

    add java doc

--- src/zombicide/actor/action/MoveAction.java
+++ src/zombicide/actor/action/MoveAction.java
@@ -2,22 +2,42 @@ package zombicide.actor.action;
 
 import zombicide.*;
 
-
+/**
+ * Represents an action for moving an Actor in a specified direction.
+ */
 public class MoveAction implements ActorAction {
 
+    /** The direction in which the Actor will move. */
     final ActorDirection direction;
 
+    /**
+     * Constructs a new MoveAction with the specified direction.
+     *
+     * @param d The direction in which the Actor will move.
+     */
     public MoveAction(ActorDirection d){
         this.direction = d;
     }
+
+    /**
+     * Calculates the new position of an Actor after moving in the specified direction.
+     *
+     * @param a The Actor whose position is to be calculated.
+     * @return The new Position after moving the Actor.
+     */
     public Position PositionAfterMoving(Actor a){
+        // Get the current area of the Actor
         Area actorArea = a.getArea();
+
+        // Get the current X and Y coordinates of the Actor's area
         int x = actorArea.getX();
         int y = actorArea.getY();
 
+        // Calculate the new X and Y coordinates after moving in the specified direction
         int i = x + direction.getX();
         int j = y + direction.getY();
 
+        // Create and return the new Position
         return new Position(i , j);
     }
 
