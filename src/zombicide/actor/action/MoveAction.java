513fb92aedb3b8a2e6f7e808db461f84573f568a
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-08 15:18:14 +0100

    add PositionAfterMoving method

--- src/zombicide/actor/action/MoveAction.java
+++ src/zombicide/actor/action/MoveAction.java
@@ -7,9 +7,15 @@ public class MoveAction implements ActorAction {
 
     private ActorDirection direction;
 
-    public Area move(Actor a){
+    public Position PositionAfterMoving(Actor a){
         Area actorArea = a.getArea();
-        int x
+        int x = actorArea.getX();
+        int y = actorArea.getY();
+
+        int i = x + direction.getX();
+        int j = y + direction.getY();
+
+        return new Position(i , j);
     }
 
 }
