929fb4ce3b9b1461078af3fb6fa108cfac7be1e7
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-03-10 23:04:21 +0100

    Add doSomething

--- src/zombicide/action/ActorAction.java
+++ src/zombicide/action/ActorAction.java
@@ -1,4 +1,7 @@
 package zombicide.action;
 
 public interface ActorAction {
+    default void doSomething() {
+        System.out.printf("%nHey ! '%s' called here !%n", getClass().getSimpleName());
+    }
 }
