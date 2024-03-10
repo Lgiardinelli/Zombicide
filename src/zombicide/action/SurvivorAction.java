5ac3943e8d9e72b1e34727deceb19fb966ce7208
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-03-10 23:03:37 +0100

    add doSomething method

--- src/zombicide/action/SurvivorAction.java
+++ src/zombicide/action/SurvivorAction.java
@@ -1,7 +1,4 @@
 package zombicide.action;
 
 public interface SurvivorAction extends ActorAction {
-    default void doSomething() {
-        System.out.printf("%nHey ! '%s' called here !%n", getClass().getSimpleName());
-    }
 }
