7ad238e2b9395c04914dccbf0afc2853f8d240c2
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-03-10 23:01:05 +0100

    Modify Main for livrable 2

--- src/zombicide/Main.java
+++ src/zombicide/Main.java
@@ -1,20 +1,27 @@
 package zombicide;
 
 import zombicide.action.SurvivorAction;
+import zombicide.action.actor.MoveAction;
 import zombicide.action.survivor.*;
 import zombicide.actor.survivor.Survivor;
-import zombicide.actor.zombie.Zombie;
 import zombicide.actor.zombie.Abomination;
+import zombicide.actor.zombie.Zombie;
+import zombicide.backpack.BackPack;
 import zombicide.city.City;
 import zombicide.city.TrainCity;
+import zombicide.item.Map;
+import zombicide.item.careItem.HealingFiask;
 import zombicide.listchooser.RandomListChooser;
-import zombicide.util.DoorDirection;
 import zombicide.role.Fighter;
 import zombicide.role.Healer;
 import zombicide.role.Lucky;
 import zombicide.role.Snooper;
+import zombicide.util.ActorDirection;
+import zombicide.util.DoorDirection;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Main {
 
@@ -44,10 +51,10 @@ public class Main {
 		city.getAreas()[0][1].getDoor(DoorDirection.DOWN).open();
 		city.getAreas()[0][1].getDoor(DoorDirection.LEFT).open();
 
-		city.getSpawn().addSurvivor(new Survivor(new Fighter()));
-		city.getSpawn().addSurvivor(new Survivor(new Healer()));
-		city.getSpawn().addSurvivor(new Survivor(new Lucky()));
-		city.getSpawn().addSurvivor(new Survivor(new Snooper()));
+		new Survivor(new Fighter()).setArea(city.getSpawn());
+		new Survivor(new Healer()).setArea(city.getSpawn());
+		new Survivor(new Lucky()).setArea(city.getSpawn());
+		new Survivor(new Snooper()).setArea(city.getSpawn());
 
 		city.display();
 	}
@@ -61,31 +68,46 @@ public class Main {
 		trainCity.getAreas()[0][1].getDoor(DoorDirection.DOWN).open();
 		trainCity.getAreas()[0][1].getDoor(DoorDirection.LEFT).open();
 
-		trainCity.getSpawn().addSurvivor(new Survivor(new Fighter()));
-		trainCity.getSpawn().addSurvivor(new Survivor(new Healer()));
-		trainCity.getSpawn().addSurvivor(new Survivor(new Lucky()));
-		trainCity.getSpawn().addSurvivor(new Survivor(new Snooper()));
+		Survivor fighter = new Survivor(new Fighter());
+		Survivor healer = new Survivor(new Healer());
+		Survivor lucky = new Survivor(new Lucky());
+		Survivor snooper = new Survivor(new Snooper());
 
-		for (int i = 0; i < trainCity.getHeight(); i++) {
-			for (int j = 0; j < trainCity.getWidth(); j++) {
-				Zombie abomination = new Abomination();
-				trainCity.getAreas()[j][i].addZombie(abomination);
-			}
-		}
-
-		//TODO Mettre une carte dans le sac à dos de chaques survivants.
-		//TODO Mettre dans la main de chaque survivant une fiole.
-
-		trainCity.display();
+		fighter.setArea(trainCity.getSpawn());
+		healer.setArea(trainCity.getSpawn());
+		lucky.setArea(trainCity.getSpawn());
+		snooper.setArea(trainCity.getSpawn());
 
 		List<Survivor> survivors = trainCity.getSpawn().getSurvivors();
 
+		// Give healing fiask to survivors
+		giveHealingFiask(survivors);
+		// Add map to
+		addMapInBackPack(survivors);
+
+		// Adding abominations in all city's areas
+		for (int i = 0; i < trainCity.getHeight(); i++) {
+			for (int j = 0; j < trainCity.getWidth(); j++) {
+				Zombie abomination = new Abomination();
+				abomination.setArea(trainCity.getAreas()[j][i]);
+			}
+		}
+
+		trainCity.display();
+
+		// Displaying survivors with their roles, their items in hand and a map in their backpack.
 		System.out.println();
 		int i = 1;
 		for (Survivor survivor : survivors) {
-			System.out.printf("Survivant %d : %s%n", i++, survivor.getRoles());
+			System.out.printf("Survivant %d : Rôles : %s | Item en main : %s | Contenu sac : %s%n",
+					i++,
+					survivor.getRoles(),
+					survivor.getHandleItem(),
+					survivor.getBackpack().getItems()
+			);
 		}
 
+		// Displaying items in some rooms
 		System.out.println();
 		trainCity.getRooms().get(0).displayItems();
 		trainCity.getRooms().get(1).displayItems();
@@ -93,11 +115,21 @@ public class Main {
 
 		System.out.println("Plateau d'entraînement avec les survivants montés de 1 case :");
 
-		for (Survivor surv : survivors) {
-			//TODO Ajout du déplacement vers le haut.
-		}
+		for (int ignored = 0; ignored < 4; ignored++)
+			new MoveAction(ActorDirection.UP, survivors.get(0), trainCity)
+					.doSomething();
 
 		trainCity.display();
+	}
+
+	private void addMapInBackPack(List<Survivor> survivors) {
+		for (Survivor survivor : survivors)
+			survivor.getBackpack().addItem(new Map());
+	}
+
+	private void giveHealingFiask(List<Survivor> survivors) {
+		for (Survivor survivor : survivors)
+			survivor.handleItem(new HealingFiask());
 	}
 
 	private void chooseRandomSurvivorAction() {
