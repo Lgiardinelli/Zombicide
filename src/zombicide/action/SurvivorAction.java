c4422535a96ac53f25a94ac217c744d14d185c8d
dylanlmr <xeskragd@gmail.com>
2024-03-10 18:40:12 +0100

    add doSomething method for debug

--- src/zombicide/action/SurvivorAction.java
+++ src/zombicide/action/SurvivorAction.java
@@ -1,5 +1,7 @@
 package zombicide.action;
 
 public interface SurvivorAction extends ActorAction {
-
+    default void doSomething() {
+        System.out.printf("%nHey ! '%s' called here !%n", getClass().getSimpleName());
+    }
 }
