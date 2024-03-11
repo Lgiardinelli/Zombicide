6a0507781811db2da40d6594687605c78386cd34
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-11 09:03:52 +0100

    update constructor

--- src/zombicide/item/Map.java
+++ src/zombicide/item/Map.java
@@ -1,13 +1,22 @@
 package zombicide.item;
 
+import zombicide.city.City;
+
 public class Map implements Item {
 	public boolean isNoisy = true;
 
-	public Map() {
+	private City board;
 
+	public Map(City c) {
+		this.board = c;
 	}
 
 	public String toString(){
 		return "map";
+	}
+
+	@Override
+	public void use() {
+
 	}
 }
