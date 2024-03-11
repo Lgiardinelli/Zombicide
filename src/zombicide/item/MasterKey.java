02d34af7c0798b881bc99b46762dbe45da1ef174
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-11 11:20:38 +0100

    update use method and remove some other methods

--- src/zombicide/item/MasterKey.java
+++ src/zombicide/item/MasterKey.java
@@ -3,6 +3,7 @@ package zombicide.item;
 import zombicide.City;
 import zombicide.Door;
 import zombicide.action.SurvivorAction;
+import zombicide.action.survivor.DoorAction;
 import zombicide.actor.survivor.Survivor;
 import zombicide.listchooser.RandomListChooser;
 import zombicide.util.Direction;
@@ -28,28 +29,9 @@ public class MasterKey implements Item {
 
 	@Override
 	public void use() {
-		List<Door> doors = doorsArround();
-		RandomListChooser<Door> chooser = new RandomListChooser<>();
-		Door door = chooser.choose(doors);
-		door.open();
+		DoorAction d = new DoorAction(survivor,city);
+		d.openTheDoor();
 	}
 
-	public List<Door> doorsArround(){
 
-		List<Door> doors = new ArrayList<>();
-		for(Direction d : Direction.values()){
-			int i = d.getX();
-			int j = d.getY();
-
-			int x = survivor.getArea().getX();
-			int y = survivor.getArea().getY();
-
-			Door door = this.city.getAreas()[y+j][x+i].getDoor(d.getReverse());
-
-			if(door != null) {
-				doors.add(door);
-			}
-		}
-		return doors;
-	}
 }
