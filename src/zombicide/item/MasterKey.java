c3d20c28eb9ce452005987a46e8c080e12fc7e1c
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-11 10:42:09 +0100

    add verticalDirection method

--- src/zombicide/item/MasterKey.java
+++ src/zombicide/item/MasterKey.java
@@ -1,16 +1,21 @@
 package zombicide.item;
 
+import zombicide.City;
+import zombicide.Door;
 import zombicide.actor.survivor.Survivor;
 import zombicide.util.Direction;
+
+import java.util.List;
 
 public class MasterKey implements Item {
 	public boolean isNoisy = false;
 
 	public Survivor survivor;
+	public City city;
 
-
-	public MasterKey(Survivor s) {
+	public MasterKey(Survivor s, City c) {
 		this.survivor=s;
+		this.city = c;
 	}
 
 	public String toString(){
@@ -19,12 +24,19 @@ public class MasterKey implements Item {
 
 	@Override
 	public void use() {
+
+	}
+
+	public List<Door> doorsArround(){
 		for(Direction d : Direction.values()){
 			int i = d.getX();
 			int j = d.getY();
 
 			int x = survivor.getArea().getX();
 			int y = survivor.getArea().getY();
+
+			int rx = d.getReversex();
+			int ry = d.getReversey();
 
 
 		}
