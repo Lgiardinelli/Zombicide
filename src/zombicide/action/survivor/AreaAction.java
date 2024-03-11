2f850f520496344dac2b31f1826450b0f8dad6b6
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-11 11:23:21 +0100

    add attributes and constructor

--- src/zombicide/action/survivor/AreaAction.java
+++ src/zombicide/action/survivor/AreaAction.java
@@ -1,13 +1,41 @@
 package zombicide.action.survivor;
 
+import zombicide.Area;
+import zombicide.City;
+import zombicide.Door;
 import zombicide.action.SurvivorAction;
+import zombicide.actor.survivor.Survivor;
+import zombicide.util.Direction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AreaAction implements SurvivorAction {
 
-    /**
-     * TODO
-     */
-    public boolean lookAround(){
-        return true;
+    private Survivor survivor;
+    private City city;
+
+    public AreaAction(Survivor s,City c){
+        this.survivor = s;
+        this.city = c;
+    }
+
+    private List<Door> areaArround(){
+
+        List<Area> areas = new ArrayList<>();
+        for(Direction d : Direction.values()){
+            int i = d.getX();
+            int j = d.getY();
+
+            int x = survivor.getArea().getX();
+            int y = survivor.getArea().getY();
+
+            Door door = this.city.getAreas()[y+j][x+i].getDoor(d.getReverse());
+
+            if(door != null) {
+                doors.add(door);
+            }
+        }
+        return doors;
     }
 }
