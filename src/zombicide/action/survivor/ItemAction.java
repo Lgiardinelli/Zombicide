1f8b7313a79b15d87dfca25559dcf8723ffe2237
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-11 08:54:11 +0100

    add attributes and constructor

--- src/zombicide/action/survivor/ItemAction.java
+++ src/zombicide/action/survivor/ItemAction.java
@@ -1,13 +1,17 @@
 package zombicide.action.survivor;
 
 import zombicide.action.SurvivorAction;
+import zombicide.item.Item;
 
 public class ItemAction implements SurvivorAction {
 
-    /**
-     * TODO
-     */
-    public boolean useEquipement(){
-        return true;
+    private Item itemUsed;
+
+    public ItemAction(Item i){
+        this.itemUsed = i;
+    }
+    @Override
+    public void doSomething(){
+
     }
 }
