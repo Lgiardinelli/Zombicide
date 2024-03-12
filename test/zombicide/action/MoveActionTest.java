a380a7af40bf45f9de82090b57b276e6fc9ea7dd
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-12 18:33:43 +0100

    add doSomething test

---
+++ test/zombicide/action/MoveActionTest.java
@@ -0,0 +1,51 @@
+package zombicide.action;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import zombicide.city.City;
+import zombicide.actor.zombie.Zombie;
+import zombicide.util.Position;
+import zombicide.actor.survivor.Survivor;
+import zombicide.actor.zombie.Walker;
+import zombicide.action.actor.MoveAction;
+import zombicide.area.Area;
+import zombicide.area.street.Street;
+import zombicide.util.Direction;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MoveActionTest {
+
+    private MoveAction m1;
+    private MoveAction m2;
+    private Survivor s1;
+    private Zombie z1;
+    private City city;
+
+    @BeforeEach
+    public void before(){
+        city = new City(5,5);
+        s1 = new Survivor(city);
+        z1 = new Walker(city);
+        Area area = new Street(2, 3);
+        s1.setArea(area);
+        z1.setArea(area);
+        this.m1 = new MoveAction(Direction.DOWN, s1);
+        this.m2 = new MoveAction(Direction.LEFT, z1);
+    }
+
+    @Test
+    public void testPositionAfterMoving(){
+        Position p1 = m1.positionAfterMoving();
+        Position p2 = m2.positionAfterMoving();
+
+        assertEquals(p1.getY(), 4);
+        assertEquals(p2.getX(), 1);
+    }
+
+    @Test
+    public void testDoSomething(){
+        m1.doSomething();
+        assertEquals(s1.getArea(), city.getArea(new Position(2,4)));
+    }
+}
