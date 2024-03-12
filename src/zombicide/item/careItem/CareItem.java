553fd3908e0220ea357961ea925a18a5ed8477c3
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-12 11:33:15 +0100

    add use method

--- src/zombicide/item/careItem/CareItem.java
+++ src/zombicide/item/careItem/CareItem.java
@@ -1,7 +1,17 @@
 package zombicide.item.careItem;
 
+import zombicide.actor.survivor.Survivor;
 import zombicide.item.Item;
 
 public abstract class CareItem implements Item {
 	public boolean isNoisy = false;
+
+
+	private Survivor survivor;
+
+
+	@Override
+	public void use() {
+
+	}
 }
