63380ec6c06220bdb3d1d0c1e73b149490032b33
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-13 16:43:54 +0100

    add heal and doSomething methods

--- src/zombicide/action/survivor/HealAction.java
+++ src/zombicide/action/survivor/HealAction.java
@@ -6,16 +6,19 @@ import zombicide.actor.survivor.Survivor;
 public class HealAction implements SurvivorAction {
 
     private final Survivor survivor;
+    private int lifePointsToAdd;
 
-    public HealAction(Survivor s){
+    public HealAction(Survivor s , int n){
         this.survivor =s;
+        this.lifePointsToAdd = n;
     }
 
     public void doSomething(){
-
+        this.heal(this.survivor);
     }
 
     public void heal(Survivor s){
-        s.
+        s.addLifePoints(this.lifePointsToAdd);
     }
+    
 }
