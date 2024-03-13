50bacad8d570abf5b9d92b83c094ffc31fe52b9c
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-13 16:38:41 +0100

    add constructor

---
+++ src/zombicide/action/survivor/HealAction.java
@@ -0,0 +1,21 @@
+package zombicide.action.survivor;
+
+import zombicide.action.SurvivorAction;
+import zombicide.actor.survivor.Survivor;
+
+public class HealAction implements SurvivorAction {
+
+    private final Survivor survivor;
+
+    public HealAction(Survivor s){
+        this.survivor =s;
+    }
+
+    public void doSomething(){
+
+    }
+
+    public void heal(Survivor s){
+        s.
+    }
+}
