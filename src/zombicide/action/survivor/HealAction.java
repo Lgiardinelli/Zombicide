57450cd74847802e403067083f466d972b3545e2
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-14 11:10:32 +0100

    add setter

--- src/zombicide/action/survivor/HealAction.java
+++ src/zombicide/action/survivor/HealAction.java
@@ -8,13 +8,16 @@ public class HealAction implements SurvivorAction {
     private final Survivor survivor;
     private int lifePointsToAdd;
 
-    public HealAction(Survivor s , int n){
+    public HealAction(Survivor s){
         this.survivor =s;
-        this.lifePointsToAdd = n;
     }
 
     public void doSomething(){
         this.heal(this.survivor);
+    }
+
+    public void setLifePointsToAdd(int n){
+        this.lifePointsToAdd = n;
     }
 
     public void heal(Survivor s){
