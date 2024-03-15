8d9cc80d87a982f40a9cb5673ed15fe855d97de6
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-15 17:03:30 +0100

    add attributes and constructor

--- src/zombicide/action/survivor/BackPackAction.java
+++ src/zombicide/action/survivor/BackPackAction.java
@@ -1,9 +1,18 @@
 package zombicide.action.survivor;
 
 import zombicide.action.SurvivorAction;
+import zombicide.actor.survivor.Survivor;
 
 public class BackPackAction implements SurvivorAction {
-    public boolean takeInHand(){
-        return true;
+
+    private Survivor survivor;
+
+    public BackPackAction(Survivor s){
+        this.survivor = s;
+    }
+    @Override
+    public void doSomething() {
+        System.out.printf("%nHey ! '%s' called here !%n", getClass().getSimpleName());
+
     }
 }
