dbb7d02f480b36e190aec36cf7b2d8947ab96a97
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-03-18 16:57:05 +0100

    Add class lookAction

--- src/zombicide/action/survivor/LookAction.java
+++ src/zombicide/action/survivor/LookAction.java
@@ -1,9 +1,17 @@
 package zombicide.action.survivor;
 
-import zombicide.action.ActorAction;
 import zombicide.action.SurvivorAction;
+import zombicide.actor.survivor.Survivor;
+import zombicide.area.Area;
+import zombicide.util.Direction;
 
 public class LookAction implements SurvivorAction {
+
+    private Survivor survivor;
+
+    public LookAction(Survivor survivor) {
+        this.survivor = survivor;
+    }
 
     public void doSomething(){
         System.out.printf("%nHey ! '%s' called here !%n", getClass().getSimpleName());
@@ -11,6 +19,11 @@ public class LookAction implements SurvivorAction {
     }
 
     private void rummage() {
+        Area a = this.survivor.getArea();
+        a.displayActors();
+        for (Direction d : Direction.values()) {
+            System.out.println("La porte " + d.name() + " est " + a.getDoor(d).isOpen());
+        }
 
     }
 
