610d00fbb54d10c13e79579187f67982f5efb7c3
dylan.lemaire2.etu <dylan.lemaire2.etu@univ-lille.fr>
2024-03-18 17:44:26 +0100

    move package

--- test/zombicide/BackPackTest.java
+++ test/zombicide/BackPackTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import zombicide.backpack.BackPack;
+import zombicide.actor.survivor.backpack.BackPack;
 import zombicide.item.InfraredGlasses;
 import zombicide.item.MasterKey;
 import zombicide.item.careItem.FirstAidKit;
