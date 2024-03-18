d4c9496e247c2d4ed7af066a24f4000375f44569
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-03-18 08:08:40 +0100

    Add constructor and update method doSomething

--- src/zombicide/action/survivor/RoomAction.java
+++ src/zombicide/action/survivor/RoomAction.java
@@ -1,8 +1,18 @@
 package zombicide.action.survivor;
 
 import zombicide.action.SurvivorAction;
+import zombicide.actor.zombie.Zombie;
+import zombicide.actor.survivor.Survivor;
+
+import java.util.List;
 
 public class RoomAction implements SurvivorAction {
+
+    private Survivor survivor;
+
+    public RoomAction(Survivor s) {
+        this.survivor = s;
+    }
 
     /**
      * TODO
@@ -15,5 +25,6 @@ public class RoomAction implements SurvivorAction {
     @Override
     public void doSomething() {
         System.out.printf("%nHey ! '%s' called here !%n", getClass().getSimpleName());
+        this.survivor.getArea().displayActors();
     }
 }
