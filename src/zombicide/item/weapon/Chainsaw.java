16a39f6e16a75e3c9f7b7adb3313efa43c61df87
dylan.lemaire2.etu <dylan.lemaire2.etu@univ-lille.fr>
2024-03-18 11:11:02 +0100

    extract init values

--- src/zombicide/item/weapon/Chainsaw.java
+++ src/zombicide/item/weapon/Chainsaw.java
@@ -1,10 +1,15 @@
 package zombicide.item.weapon;
 
 public class Chainsaw extends Weapon{
-	
+	private static final int NB_DICE_THROWS = 2;
+	private static final int DICE_THRESHOLD = 5;
+	private static final int DAMAGE = 3;
+	private static final int MIN_HITTING_RANGE = 0;
+	private static final int MAX_HITTING_RANGE = 0;
+	private static final boolean IS_NOISY = true;
 	
 	public Chainsaw() {
-		super(2,5,3,0,0,true);
+		super(NB_DICE_THROWS, DICE_THRESHOLD, DAMAGE, MIN_HITTING_RANGE, MAX_HITTING_RANGE, IS_NOISY);
 	}
 
 	public String toString(){
@@ -13,6 +18,5 @@ public class Chainsaw extends Weapon{
 
 	@Override
 	public void use() {
-
 	}
 }
