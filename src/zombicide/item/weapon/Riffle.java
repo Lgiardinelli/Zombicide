2c8cb83324792edf3c10a2bc0b5a32fe585d2ea9
dylan.lemaire2.etu <dylan.lemaire2.etu@univ-lille.fr>
2024-03-18 11:11:29 +0100

    extract init values

--- src/zombicide/item/weapon/Riffle.java
+++ src/zombicide/item/weapon/Riffle.java
@@ -1,12 +1,16 @@
 package zombicide.item.weapon;
 
 public class Riffle extends Weapon{
-	
+	private static final int NB_DICE_THROWS = 2;
+	private static final int DICE_THRESHOLD = 4;
+	private static final int DAMAGE = 1;
+	private static final int MIN_HITTING_RANGE = 1;
+	private static final int MAX_HITTING_RANGE = 3;
+	private static final boolean IS_NOISY = true;
 	
 	public Riffle() {
-		super(2,4,1,1,3,true);
+		super(NB_DICE_THROWS, DICE_THRESHOLD, DAMAGE, MIN_HITTING_RANGE, MAX_HITTING_RANGE, IS_NOISY);
 	}
-
 
 	public String toString(){
 		return "riffle";
@@ -14,6 +18,5 @@ public class Riffle extends Weapon{
 
 	@Override
 	public void use() {
-
 	}
 }
