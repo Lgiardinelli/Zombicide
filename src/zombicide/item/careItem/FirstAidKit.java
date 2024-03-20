4731fac8369edae208a9657f8bb2e4be63d853dd
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-20 16:58:24 +0100

    add chooseAPlayer method

--- src/zombicide/item/careItem/FirstAidKit.java
+++ src/zombicide/item/careItem/FirstAidKit.java
@@ -1,5 +1,13 @@
 package zombicide.item.careItem;
 
+import zombicide.actor.action.HealAction;
+import zombicide.actor.survivor.Survivor;
+import zombicide.city.area.Area;
+import zombicide.util.listchooser.ListChooser;
+import zombicide.util.listchooser.RandomListChooser;
+
+import java.util.List;
+
 public class FirstAidKit extends CareItem {
 
     public String toString(){
@@ -8,6 +16,17 @@ public class FirstAidKit extends CareItem {
 
     @Override
     public void use() {
-
+        HealAction h = new HealAction(chooseAPlayer());
+        h.doSomething();
     }
+
+    public Survivor chooseAPlayer(){
+        Area a = this.survivor.getArea();
+        List<Survivor> survivors = a.getSurvivors();
+        ListChooser<Survivor> chooser = new RandomListChooser<>();
+        Survivor s = chooser.choose(survivors);
+        return  s;
+    }
+
+
 }
