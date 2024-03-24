a15385973f71457751360cbd281f0a137186927d
dylanlmr <xeskragd@gmail.com>
2024-03-24 17:13:37 +0100

    add AreaActionTest.java

---
+++ test/zombicide/actor/action/AreaActionTest.java
@@ -0,0 +1,21 @@
+package zombicide.actor.action;
+
+import org.junit.jupiter.api.Test;
+import zombicide.actor.survivor.Survivor;
+import zombicide.city.City;
+import zombicide.city.area.room.Room;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AreaActionTest {
+
+    @Test
+    public void testDoSomething() {
+        Survivor s = new Survivor(new City(10, 10));
+        s.setArea(new Room(5, 4));
+        AreaAction action = new AreaAction(s);
+        int actionPoints = s.getActionPoints();
+        action.doSomething();
+        assertEquals(actionPoints - 1, s.getActionPoints());
+    }
+}
