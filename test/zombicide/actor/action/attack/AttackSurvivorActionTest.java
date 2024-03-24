ea569cb4b5278774f9a6c88c1b20ffa5a30bc625
dylanlmr <xeskragd@gmail.com>
2024-03-24 17:17:46 +0100

    implements AttackSurvivorActionTest.java

---
+++ test/zombicide/actor/action/attack/AttackSurvivorActionTest.java
@@ -0,0 +1,71 @@
+package zombicide.actor.action.attack;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import zombicide.actor.survivor.Survivor;
+import zombicide.actor.zombie.Abomination;
+import zombicide.actor.zombie.Zombie;
+import zombicide.city.City;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AttackSurvivorActionTest {
+    private City city;
+    private Zombie zombie;
+    private Survivor survivor;
+    private AttackSurvivorAction action;
+
+    @BeforeEach
+    public void setup() {
+        city = new City(10, 10);
+        zombie = new Abomination(city);
+        survivor = new Survivor(city);
+        action = new AttackSurvivorAction(zombie);
+    }
+
+    @Test
+    public void testDoSomethingWhenOnlyOneSurvivorIsPresent() {
+        zombie.setArea(city.getArea(0, 0));
+        survivor.setArea(city.getArea(0, 0));
+
+        int initialLifePoints = survivor.getLifePoints();
+        int attackPoints = zombie.getAttackPoints();
+
+        action.doSomething();
+
+        assertEquals(initialLifePoints - attackPoints, survivor.getLifePoints());
+    }
+
+    @Test
+    public void testDoSomethingWhenMultipleSurvivorsArePresent() {
+        zombie.setArea(city.getArea(0, 0));
+        survivor.setArea(city.getArea(0, 0));
+        Survivor survivor2 = new Survivor(city);
+        survivor2.setArea(city.getArea(0, 0));
+
+        int initialLifePoints = survivor.getLifePoints();
+        int initialLifePoints2 = survivor2.getLifePoints();
+        int attackPoints = zombie.getAttackPoints();
+
+        action.doSomething();
+
+        boolean isSurvivor1Attacked = survivor.getLifePoints() == initialLifePoints - attackPoints;
+        boolean isSurvivor2Attacked = survivor2.getLifePoints() == initialLifePoints2 - attackPoints;
+
+        assertTrue((isSurvivor1Attacked && !isSurvivor2Attacked) ||
+                (!isSurvivor1Attacked && isSurvivor2Attacked));
+    }
+
+    @Test
+    public void testDoSomethingWhenNoSurvivorArePresent() {
+        zombie.setArea(city.getArea(0, 0));
+        survivor.setArea(city.getArea(0, 1));
+
+        int initialLifePoints = survivor.getLifePoints();
+
+        action.doSomething();
+
+        assertEquals(initialLifePoints, survivor.getLifePoints());
+    }
+}
