8883801755a6429380aa1a9cc173f539c411b462
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-03-25 09:35:18 +0100

    modify constructor

--- src/zombicide/item/weapon/Riffle.java
+++ src/zombicide/item/weapon/Riffle.java
@@ -6,7 +6,6 @@ public class Riffle extends Weapon{
 	private static final int DAMAGE = 1;
 	private static final int MIN_HITTING_RANGE = 1;
 	private static final int MAX_HITTING_RANGE = 3;
-	private static final boolean IS_NOISY = true;
 	
 	public Riffle() {
 		super(NB_DICE_THROWS, DICE_THRESHOLD, DAMAGE, MIN_HITTING_RANGE, MAX_HITTING_RANGE);
