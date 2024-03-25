e553d5c14c93f848ddf0de75967d884c11f99724
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-03-25 09:11:36 +0100

    Modify testAddItem to testAddItemAndCheckIfBackpackWasFull

--- test/zombicide/actor/survivor/backpack/BackPackTest.java
+++ test/zombicide/actor/survivor/backpack/BackPackTest.java
@@ -5,7 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import zombicide.actor.survivor.Survivor;
+import zombicide.actor.survivor.role.Fighter;
+import zombicide.actor.survivor.role.Role;
+import zombicide.city.City;
 import zombicide.item.InfraredGlasses;
+import zombicide.item.Item;
 import zombicide.item.MasterKey;
 import zombicide.item.careItem.FirstAidKit;
 import zombicide.item.weapon.Chainsaw;
@@ -15,18 +20,13 @@ import zombicide.item.weapon.Riffle;
 class BackPackTest {
 
     private BackPack backpack;
-
     private Pistol item1;
     private FirstAidKit item2;
-
     private Riffle item3;
-
     private MasterKey item4;
-
     private InfraredGlasses item5;
-
     private Chainsaw item6;
-
+    private Survivor survivor;
 
     @BeforeEach
     public void before() {
@@ -37,22 +37,23 @@ class BackPackTest {
         item4 = new MasterKey();
         item5 = new InfraredGlasses();
         item6 = new Chainsaw();
+        survivor = new Survivor(new City(5, 5), new Fighter());
     }
+
     @Test
-    void testAddItem(){
+    void testAddItemAndCheckIfBackpackWasFull(){
+        assertNull(backpack.addItem(item1));
+        assertNull(backpack.addItem(item2));
+        assertNull(backpack.addItem(item3));
+        assertNull(backpack.addItem(item4));
+        assertNull(backpack.addItem(item5));
 
-
-        backpack.addItem(item1);
-        backpack.addItem(item2);
-        backpack.addItem(item3);
-        backpack.addItem(item4);
-        backpack.addItem(item5);
-
-        assertTrue(backpack.getItems().contains(item3));
-
-        assertFalse(backpack.getItems().contains(item6));
-
-
+        Item itemDropped = backpack.addItem(item6);
+        assertNotNull(itemDropped);
+        assertTrue(itemDropped.equals(item1) || itemDropped.equals(item2) || itemDropped.equals(item3) || itemDropped.equals(item4) || itemDropped.equals(item5));
+        assertTrue(backpack.getItems().contains(item6));
+        assertFalse(backpack.getItems().contains(itemDropped));
+        assertNull(itemDropped.getSurvivor());
     }
 
     @Test
