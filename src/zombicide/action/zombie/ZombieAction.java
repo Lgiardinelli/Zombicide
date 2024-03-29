7aae148af27a8b7c6160589a0db73653559f587c
dylan.lemaire2.etu <dylan.lemaire2.etu@univ-lille.fr>
2024-03-29 15:18:26 +0100

    add action impl

--- src/zombicide/action/zombie/ZombieAction.java
+++ src/zombicide/action/zombie/ZombieAction.java
@@ -3,13 +3,6 @@ package zombicide.action.zombie;
 import zombicide.action.Action;
 import zombicide.actor.zombie.Zombie;
 
-public abstract class ZombieAction implements Action {
-    protected Zombie zombie;
-
-    @Override
-    public abstract void doSomething();
-
-    public void setZombie(Zombie zombie) {
-        this.zombie = zombie;
-    }
+public interface ZombieAction extends Action {
+    void doSomething(Zombie zombie);
 }
