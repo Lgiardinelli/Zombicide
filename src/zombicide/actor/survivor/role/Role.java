6a0b3f4f1a1961869846dd277dfa94b78e39f3e3
dylan.lemaire2.etu <dylan.lemaire2.etu@univ-lille.fr>
2024-04-03 18:21:26 +0200

    Add role interface

--- src/zombicide/actor/survivor/role/Role.java
+++ src/zombicide/actor/survivor/role/Role.java
@@ -1,4 +1,13 @@
 package zombicide.actor.survivor.role;
 
-public interface Role {
+import zombicide.action.Action;
+import zombicide.actor.survivor.Survivor;
+
+public interface Role extends Action<Survivor> {
+    default boolean extendsAction(Action<Survivor> action) {
+        return action.getClass().isAssignableFrom(this.getClass());
+    }
+
+    @Override
+    void doSomething(Survivor actor);
 }
