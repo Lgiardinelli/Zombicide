bef2ad3dc4fa504690a6ab395560fe46f58a2619
Theophane Legrand <theophane.legrand.etu@univ-lille.fr>
2024-04-04 11:40:17 +0200

    Add testShootingTarget method

--- test/zombicide/item/WeaponTest.java
+++ test/zombicide/item/WeaponTest.java
@@ -1,4 +1,32 @@
 package zombicide.item;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import zombicide.item.weapon.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class WeaponTest {
+    private Weapon pistol;
+    private Weapon axe;
+    private Weapon chainsaw;
+    private Weapon crowbar;
+    private Weapon riffle;
+
+    @BeforeEach
+    public void before(){
+        this.pistol = new Pistol();
+        this.axe = new Axe();
+        this.chainsaw = new Chainsaw();
+        this.riffle = new Riffle();
+        this.crowbar = new Crowbar();
+    }
+
+    @Test
+    void testShootingTarget(){
+        int v = pistol.shoot();
+        assertTrue(v > 0);
+        if (v >= pistol.getDiceThreshold())
+            assertTrue(pistol.shotHitsTarget(v));
+    }
 }
