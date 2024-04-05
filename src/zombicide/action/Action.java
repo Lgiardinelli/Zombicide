7eb7727b766f9ccf2a188078653c8a4d6929065b
dylan.lemaire2.etu <dylan.lemaire2.etu@univ-lille.fr>
2024-04-05 16:27:19 +0200

    Add display method

--- src/zombicide/action/Action.java
+++ src/zombicide/action/Action.java
@@ -3,8 +3,5 @@ package zombicide.action;
 import zombicide.actor.Actor;
 
 public interface Action<T extends Actor> {
-    default String display() {
-        return getClass().getSimpleName();
-    }
     void doSomething(T actor);
 }
