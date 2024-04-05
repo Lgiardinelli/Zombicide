a93c2d3e9688375d7c8894eff394a12089575694
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-04-05 16:22:55 +0200

    add lifePointsToAdd attribute

--- src/zombicide/item/careItem/CareItem.java
+++ src/zombicide/item/careItem/CareItem.java
@@ -4,9 +4,15 @@ import zombicide.actor.survivor.Survivor;
 import zombicide.item.Item;
 
 public abstract class CareItem extends Item {
+
+	protected int lifePointsToAdd;
 	public CareItem(){
 		super();
 		this.isNoisyUse = true;
+	}
+
+	public void setLifePointsToAdd(int n){
+		this.lifePointsToAdd = n;
 	}
 
 	public void use() {
