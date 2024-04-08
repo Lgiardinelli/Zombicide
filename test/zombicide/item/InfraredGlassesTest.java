be176ae23f402c7f7b6239b9a774bc4376ab8f2a
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-04-08 09:03:39 +0200

    add useTest

--- test/zombicide/item/InfraredGlassesTest.java
+++ test/zombicide/item/InfraredGlassesTest.java
@@ -2,6 +2,8 @@ package zombicide.item;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import zombicide.actor.survivor.Survivor;
+import zombicide.city.City;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,10 +11,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class InfraredGlassesTest {
 
     private InfraredGlasses infraredGlasses;
+    private Survivor s;
 
     @BeforeEach
     public void setUp() {
         infraredGlasses = new InfraredGlasses();
+        infraredGlasses.setSurvivor(new Survivor(new City(5,5)));
     }
 
     @Test
@@ -21,5 +25,10 @@ public class InfraredGlassesTest {
         assertFalse(infraredGlasses.canAttack());
         assertFalse(infraredGlasses.isNoisyWhenUsedToOpenDoor());
         assertFalse(infraredGlasses.isNoisyWhenUsed());
+    }
+
+    @Test
+    public void useTest(){
+        infraredGlasses.use();
     }
 }
