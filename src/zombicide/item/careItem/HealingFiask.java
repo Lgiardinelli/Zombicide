d3a6f711ae7a20c4c3f945adf17aa55bd56bb518
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-04-09 17:33:30 +0200

    update java doc

--- src/zombicide/item/careItem/HealingFiask.java
+++ src/zombicide/item/careItem/HealingFiask.java
@@ -2,16 +2,32 @@ package zombicide.item.careItem;
 
 import zombicide.action.survivor.HealAction;
 
+/**
+ * HealingFiask class represents a healing flask care item in the game.
+ * It allows a survivor to heal themselves when used.
+ */
 public class HealingFiask extends CareItem {
 
-    public String toString(){
+    public HealingFiask() {
+        super();
+    }
+
+    /**
+     * Returns a string representation of the HealingFiask.
+     *
+     * @return The string "healing flask".
+     */
+    public String toString() {
         return "healing flask";
     }
 
+    /**
+     * Performs the action of using the healing flask, which triggers a HealAction.
+     */
     @Override
     public void use() {
-        HealAction h = new HealAction();
-        h.setLifePointsToAdd(this.lifePointsToAdd);
-        h.doSomething(survivor);
+        HealAction healAction = new HealAction();
+        healAction.setLifePointsToAdd(this.lifePointsToAdd);
+        healAction.doSomething(survivor);
     }
 }
