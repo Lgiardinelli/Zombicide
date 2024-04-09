8a353e36a4b3392339313ba38d55c048dcec14d9
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-04-09 17:33:47 +0200

    update java doc

--- src/zombicide/action/survivor/ItemAction.java
+++ src/zombicide/action/survivor/ItemAction.java
@@ -4,10 +4,20 @@ import zombicide.action.Action;
 import zombicide.actor.survivor.Survivor;
 import zombicide.item.Item;
 
+/**
+ * An action representing a Survivor using an item they are currently holding.
+ * The Survivor can use this action to use the item currently in their hand.
+ */
 public class ItemAction implements Action<Survivor> {
 
+    /**
+     * Performs the action of using the item currently held by the Survivor.
+     * This action triggers the use method of the item and consumes an action point.
+     *
+     * @param survivor The Survivor performing the item action.
+     */
     @Override
-    public void doSomething(Survivor survivor){
+    public void doSomething(Survivor survivor) {
         Item itemUsed = survivor.getItemHeld();
         itemUsed.use();
         survivor.removeActionPoint();
