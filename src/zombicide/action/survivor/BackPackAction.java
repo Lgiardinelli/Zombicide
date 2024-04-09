0ebf87843ed64a991358bf3856683d60cd35f001
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-04-09 17:33:18 +0200

    update java doc

--- src/zombicide/action/survivor/BackPackAction.java
+++ src/zombicide/action/survivor/BackPackAction.java
@@ -4,8 +4,21 @@ import zombicide.action.Action;
 import zombicide.actor.survivor.backpack.BackPack;
 import zombicide.actor.survivor.Survivor;
 
+/**
+ * An action representing a Survivor interacting with their backpack.
+ * This action allows the Survivor to take an item from their backpack and equip it in their hand.
+ * If the Survivor already has an item in their hand, the action swaps it with a randomly chosen item from the backpack.
+ * If the Survivor's hand is empty, the action equips a randomly chosen item from the backpack.
+ */
 public class BackPackAction implements Action<Survivor> {
 
+    /**
+     * Performs the action of interacting with the Survivor's backpack.
+     * The Survivor holds a randomly chosen item from the backpack.
+     *
+     * @param survivor The Survivor performing the action.
+     */
+    @Override
     public void doSomething(Survivor survivor) {
         holdRandomItem(survivor);
         survivor.removeActionPoint();
@@ -15,11 +28,14 @@ public class BackPackAction implements Action<Survivor> {
      * Allows the survivor to take an item from their backpack and equip it in their hand.
      * If the survivor already has an item in their hand, the method swaps it with a randomly chosen item from the backpack.
      * If the survivor's hand is empty, the method equips a randomly chosen item from the backpack.
+     *
+     * @param survivor The Survivor performing the action.
      */
     private void holdRandomItem(Survivor survivor) {
         BackPack backpack = survivor.getBackpack();
-        if (backpack.getItems().isEmpty())
+        if (backpack.getItems().isEmpty()) {
             return;
+        }
 
         survivor.setItemHeld(backpack.getRandomItem());
     }
