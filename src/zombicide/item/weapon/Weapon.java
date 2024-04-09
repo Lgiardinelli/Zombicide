f6865726a4c6392c232c9a6b4258e996510f4b3a
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-04-09 17:33:49 +0200

    update java doc

--- src/zombicide/item/weapon/Weapon.java
+++ src/zombicide/item/weapon/Weapon.java
@@ -8,19 +8,28 @@ import zombicide.util.Direction;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
-
+/**
+ * Abstract class representing a Weapon item in the game.
+ * Provides methods to handle shooting range and attack mechanics.
+ */
 public abstract class Weapon extends Item {
-	private static final Random RANDOM = new Random();
-    protected int nbDiceThrows;
-    protected int diceThreshold;
-    protected int damage;
-    protected int minHittingRange;
-    protected int maxHittingRange;
+	protected int nbDiceThrows;
+	protected int diceThreshold;
+	protected int damage;
+	protected int minHittingRange;
+	protected int maxHittingRange;
 
-
-    public Weapon(int nbDiceThrows, int diceThreshold, int damage, int minHittingRange, int maxHittingRange) {
+	/**
+	 * Creates a new Weapon with specified characteristics.
+	 *
+	 * @param nbDiceThrows     The number of dice throws for the weapon.
+	 * @param diceThreshold    The threshold for a successful shot.
+	 * @param damage           The damage inflicted by the weapon.
+	 * @param minHittingRange  The minimum hitting range of the weapon.
+	 * @param maxHittingRange  The maximum hitting range of the weapon.
+	 */
+	public Weapon(int nbDiceThrows, int diceThreshold, int damage, int minHittingRange, int maxHittingRange) {
 		super();
 		this.nbDiceThrows = nbDiceThrows;
 		this.diceThreshold = diceThreshold;
@@ -30,15 +39,16 @@ public abstract class Weapon extends Item {
 		this.canAttack = true;
 		this.isNoisyDoor = true;
 	}
-    
-    /**
-     * Checks if the weapon can successfully shoot based on the dice throws and threshold.
-     *
-     * @return if the weapon can shoot successfully, otherwise.
-     */
-    public boolean shotHitsTarget(int lastShotValue) {
-        return lastShotValue >= this.diceThreshold;
-    }
+
+	/**
+	 * Checks if the weapon can successfully hit a target based on the dice throws and threshold.
+	 *
+	 * @param lastShotValue The value of the last shot rolled.
+	 * @return true if the weapon hits the target, false otherwise.
+	 */
+	public boolean shotHitsTarget(int lastShotValue) {
+		return lastShotValue >= this.diceThreshold;
+	}
 
 	public int getNbDiceThrows() {
 		return nbDiceThrows;
@@ -60,22 +70,29 @@ public abstract class Weapon extends Item {
 		return maxHittingRange;
 	}
 
+	/**
+	 * Finds the zombies within the shooting range of the weapon based on survivor's area.
+	 *
+	 * @return List of zombies within the weapon's shooting range.
+	 */
 	public List<Zombie> shootRange() {
 		Area areaWeapon = this.survivor.getArea();
 		City city = this.survivor.getCity();
 		int x = areaWeapon.getX();
 		int y = areaWeapon.getY();
 		List<Zombie> zombies = new ArrayList<>();
+
 		if (this.getMinHittingRange() == 0) {
 			List<Zombie> zom = city.getArea(x, y).getZombies();
 			zombies.addAll(zom);
 		}
+
 		for (Direction d : Direction.values()) {
 			for (int i = 1; i <= this.getMaxHittingRange(); i++) {
-				int newX = x + i*d.getX();
-				int newY = y + i*d.getY();
-				if (this.getMinHittingRange() <=i) {
-					if(newX < city.getWidth() && newX >= 0 && newY < city.getHeight() && newY >= 0){
+				int newX = x + i * d.getX();
+				int newY = y + i * d.getY();
+				if (this.getMinHittingRange() <= i) {
+					if (newX < city.getWidth() && newX >= 0 && newY < city.getHeight() && newY >= 0) {
 						if (!(areaWeapon.isContinental())) {
 							List<Zombie> zom = city.getArea(newX, newY).getZombies();
 							zombies.addAll(zom);
@@ -84,11 +101,11 @@ public abstract class Weapon extends Item {
 				}
 			}
 		}
+
 		return zombies;
 	}
 
 	public void use() {
 		System.out.println(shootRange());
 	}
-    
 }
