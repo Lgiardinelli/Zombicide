ed7cdb3420e2b677b7a7b56eca099ce2b1393bb3
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-04-09 17:33:24 +0200

    update java doc

--- src/zombicide/item/InfraredGlasses.java
+++ src/zombicide/item/InfraredGlasses.java
@@ -3,14 +3,27 @@ package zombicide.item;
 import zombicide.action.survivor.AreaAction;
 import zombicide.actor.survivor.Survivor;
 
+/**
+ * Represents Infrared Glasses item in the game.
+ * This item allows a survivor to display areas around them when used.
+ */
 public class InfraredGlasses extends Item {
 
-	public String toString(){
+	/**
+	 * Returns a string representation of the Infrared Glasses item.
+	 *
+	 * @return The string "infrared glasses".
+	 */
+	public String toString() {
 		return "infrared glasses";
 	}
 
+	/**
+	 * Uses the Infrared Glasses item to display areas around the survivor.
+	 * Creates an AreaAction and performs it on the survivor.
+	 */
 	public void use() {
-		AreaAction a = new AreaAction();
-		a.doSomething(survivor);
+		AreaAction areaAction = new AreaAction();
+		areaAction.doSomething(survivor);
 	}
 }
