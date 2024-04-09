3a76064c0333a478796f646dde6ee32f26297bc5
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-04-09 17:33:25 +0200

    update java doc

--- src/zombicide/item/MasterKey.java
+++ src/zombicide/item/MasterKey.java
@@ -3,17 +3,32 @@ package zombicide.item;
 import zombicide.action.survivor.DoorAction;
 import zombicide.actor.survivor.Survivor;
 
+/**
+ * Represents a Master Key item in the game.
+ * This item allows a survivor to open a door when used.
+ */
 public class MasterKey extends Item {
-	public MasterKey(){
+
+	public MasterKey() {
 		super();
 		this.canOpen = true;
 	}
-	public String toString(){
+
+	/**
+	 * Returns a string representation of the MasterKey item.
+	 *
+	 * @return The string "master key".
+	 */
+	public String toString() {
 		return "master key";
 	}
 
+	/**
+	 * Uses the MasterKey item to open a door.
+	 * Creates a DoorAction and performs the action using the survivor.
+	 */
 	public void use() {
-		DoorAction d = new DoorAction();
-		d.doSomething(this.survivor);
+		DoorAction doorAction = new DoorAction();
+		doorAction.doSomething(this.survivor);
 	}
 }
