5bc5207f476a9a7ec49c5358cfc902310113092a
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-04-09 17:33:16 +0200

    update java doc

--- src/zombicide/action/survivor/AreaAction.java
+++ src/zombicide/action/survivor/AreaAction.java
@@ -1,6 +1,5 @@
 package zombicide.action.survivor;
 
-;
 import zombicide.action.Action;
 import zombicide.actor.survivor.Survivor;
 import zombicide.city.City;
@@ -8,39 +7,52 @@ import zombicide.city.area.Area;
 import zombicide.city.area.room.Room;
 import zombicide.util.Direction;
 
+/**
+ * An action representing a Survivor inspecting the areas around their current position.
+ * This action displays the actors and items in adjacent areas.
+ */
 public class AreaAction implements Action<Survivor> {
 
     /**
-     * TODO
-     * @return boolean
+     * Displays the actors and items in the areas surrounding the Survivor's current position.
+     *
+     * @param survivor The Survivor performing the action.
      */
-    private void displayAreasAround(Survivor survivor){
+    private void displayAreasAround(Survivor survivor) {
         City city = survivor.getCity();
-        for(Direction d : Direction.values()){
-            // System.out.print(d.name()+" :");
+        for (Direction d : Direction.values()) {
             int i = d.getX();
             int j = d.getY();
 
             int x = survivor.getArea().getX();
             int y = survivor.getArea().getY();
 
-            Area a = city.getAreas()[y+j][x+i];
+            Area a = city.getAreas()[y + j][x + i];
 
             a.displayActors();
             System.out.println();
-            if(!a.isARoom())
+
+            if (!a.isARoom()) {
                 return;
+            }
+
             Room r = (Room) a;
-            if (city.containsRoom(r))
+            if (city.containsRoom(r)) {
                 r.displayItems();
-            System.out.println();
+                System.out.println();
+            }
         }
     }
 
+    /**
+     * Performs the action of displaying areas around the Survivor.
+     * This action consumes one action point from the Survivor.
+     *
+     * @param survivor The Survivor performing the action.
+     */
     @Override
     public void doSomething(Survivor survivor) {
         displayAreasAround(survivor);
         survivor.removeActionPoint();
     }
 }
-
