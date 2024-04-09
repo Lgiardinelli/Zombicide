acc552879130428eb56fb146f3374a8937332969
elies.placide.etu <elies.placide.etu@univ-lille.fr>
2024-04-09 17:33:27 +0200

    update java doc

--- src/zombicide/action/survivor/NoiseAction.java
+++ src/zombicide/action/survivor/NoiseAction.java
@@ -4,25 +4,32 @@ import zombicide.action.Action;
 import zombicide.actor.survivor.Survivor;
 import zombicide.city.area.Area;
 
+/**
+ * An action representing a Survivor creating noise in their current area.
+ * This action increases the noise level in the Survivor's current area.
+ */
 public class NoiseAction implements Action<Survivor> {
 
     private int noiseLevel;
 
-    public void setLifePointsToAdd(int n){
-        this.noiseLevel = n;
-    }
-
+    /**
+     * Sets the noise level to be added when performing the noise action.
+     *
+     * @param noiseLevel The noise level to add.
+     */
     public void setNoiseLevel(int noiseLevel) {
         this.noiseLevel = noiseLevel;
     }
 
     /**
-     * Increases the noise level in the current area where the survivor is located.
-     * If the survivor is not in any area, no noise level is increased.
+     * Performs the action of creating noise in the Survivor's current area.
+     * This action increases the noise level of the area.
+     *
+     * @param survivor The Survivor creating noise.
      */
-    public void doSomething(Survivor survivor){
+    public void doSomething(Survivor survivor) {
         Area currentArea = survivor.getArea();
-        if(currentArea != null){
+        if (currentArea != null) {
             currentArea.increaseNoiseLevel(this.noiseLevel);
         }
         survivor.removeActionPoint();
