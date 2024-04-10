d37536474570e4b0fdf3c16d334aac3d8e7c5213
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-04-10 13:39:32 +0200

    Add toString method

--- src/zombicide/action/survivor/ItemAction.java
+++ src/zombicide/action/survivor/ItemAction.java
@@ -22,4 +22,8 @@ public class ItemAction implements Action<Survivor> {
         itemUsed.use();
         survivor.removeActionPoint();
     }
+
+    public String toString() {
+        return "Item action";
+    }
 }
