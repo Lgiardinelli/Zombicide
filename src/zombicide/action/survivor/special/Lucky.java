bf2654288a19c7488fa894d65f7b3e5c92d6bd48
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-04-14 03:26:53 +0200

    Add override method for lucky role

--- src/zombicide/action/survivor/special/Lucky.java
+++ src/zombicide/action/survivor/special/Lucky.java
@@ -2,11 +2,18 @@ package zombicide.action.survivor.special;
 
 import zombicide.action.survivor.AttackZombieAction;
 import zombicide.actor.survivor.Survivor;
+import zombicide.actor.zombie.Zombie;
+import zombicide.item.weapon.Weapon;
 
 public class Lucky extends AttackZombieAction {
 
     @Override
-    public void doSomething(Survivor survivor) {
+    protected void shootZombie(Survivor survivor, Zombie zombie, Weapon weapon) {
+        System.out.println("ici");
+        int highestDieValue = getHighestDieValue(weapon.getNbDiceThrows() + 1);
 
+        if (weapon.shotHitsTarget(highestDieValue)) {
+            attackZombie(survivor, zombie, weapon);
+        }
     }
 }
