25e306d035bc4d89c1f4fc6709e874cedb093066
leo.giardinelli.etu <leo.giardinelli.etu@univ-lille.fr>
2024-04-14 04:02:37 +0200

    Update lifePoints runner

--- src/zombicide/actor/zombie/Runner.java
+++ src/zombicide/actor/zombie/Runner.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Runner extends Zombie {
 	private static final int ATTACK_POINTS = 1;
-	private static final int LIFE_POINTS = 1;
+	private static final int LIFE_POINTS = 2;
 	private static final int ACTION_POINTS = 2;
 	private static final boolean IS_STRONG = false;
 
